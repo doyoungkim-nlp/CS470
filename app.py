3b3fb5b0fe867bfeee32432b02f43d24b76db8d2
반석 우 <woobanseok@hanmail.net>
2020-11-10 16:53:31 +0900

    testing code: imagebase64로 server side로 보내기(이후 server에서 classification)

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ from models import db
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 
+import numpy as np
+from PIL import Image
+import base64
+import re
+try:
+       from cStringIO import StringIO  # Py2 C accelerated version
+except ImportError:
+       try:
+           from StringIO import StringIO  # Py2 fallback version
+       except ImportError:
+           from io import StringIO  # Py3 version
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,6 +35,17 @@ def result():
 @app.route('/statistics')
 def statistics():
 	return render_template('statistics.html')
+
+""" test중... 서버 사이드에서 이미지 받기
+
+@app.route('/hook', methods=['POST'])
+def get_image():
+    image_b64 = request.values['imageBase64']
+    image_data = re.sub('^data:image/.+;base64,', '', image_b64)
+    image_PIL = Image.open(StringIO(image_b64))
+    image_np = np.array(image_PIL)
+    print ('Image received: {}'.format(image_np.shape))
+    return '' """
 
 @app.route('/fileUpload', methods = ['GET', 'POST'])
 def upload_file():
