db45bbc6e8341327eb7e94756c43d41db7559262
auswls0727 <hyunjin@DESKTOP-LR96JLP.localdomain>
2020-11-28 05:19:39 +0900

    ipynb 파일 삭제, sketch generation demo 추가, sketch recognition demo 이미지 저장 형식 변경

---
+++ sketch_generation_demo/static/js/canvas.js
@@ -0,0 +1,65 @@
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const myPics = document.getElementById("canvas");
+const saveBtn = document.getElementById("btn")
+const context = myPics.getContext('2d');
+
+// event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+myPics.addEventListener('mousedown', e => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+myPics.addEventListener('mousemove', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener('mouseup', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+saveBtn.addEventListener('click', handleSave)
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = 'white';
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+
+function handleSave() {
+  const image = canvas.toDataURL("image/jpeg", 1.0);
+/*   const link = document.createElement("a")
+  link.href = image;
+  link.download = "Paint"
+  link.click() */
+
+console.log("about to send")
+  $.ajax({
+    type: "POST",
+    url: "http://url/hook",
+    data:{
+      imageBase64: image
+    }
+  }).done(function() {
+    console.log('sent');
+  
+  });
+}
--- sketch_recognition_model/app/run.py
+++ sketch_recognition_model/app/run.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on May  10 2019
-
-@author: Aleksandra Deis
-
-Script which runs flask web application for Quick Draw
-
-"""
 #import libraries
 import numpy as np
 from PIL import Image
@@ -46,7 +36,6 @@ from torch import nn
 from torch import optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms
-
 
 from models.simple_conv_nn import SimpleCNN
 from models.RMDL import RMDL
@@ -159,7 +148,7 @@ def get_prediction(model, input):
 
     return label, label_name, preds
 
-def view_classify(img, preds):
+def view_classify(original, img, preds):
     """
     Function for viewing an image and it's predicted classes
     with matplotlib.
@@ -171,7 +160,8 @@ def view_classify(img, preds):
     preds = preds.squeeze()
 
     fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
-    ax1.imshow(img.squeeze())
+    #ax1.imshow(img.squeeze())
+    ax1.imshow(original)
     ax1.axis('off')
     ax2.barh(np.arange(10), preds)
     ax2.set_aspect(0.1)
@@ -219,10 +209,10 @@ def pred(dataURL):
 
     # save original image as png (for debugging)
     ts = time.time()
-    #img.save('image' + str(ts) + '.png', 'PNG')
+    img.save('image' + str(ts) + '.png', 'PNG')
 
     # convert image to RGBA
-    img = img.convert("RGBA")
+    #img = img.convert("RGBA")
 
     # preprocess the image for the model
     image_cropped = crop_image(img) # crop the image and resize to 28x28
@@ -239,7 +229,7 @@ def pred(dataURL):
     print("This is a {}".format(label_num))
 
     # save classification results as a diagram
-    view_classify(image_np, preds)
+    view_classify(img, image_np, preds)
 
     # create plotly visualization
     graphs = [
